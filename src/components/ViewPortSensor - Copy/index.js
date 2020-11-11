1c4c0aa008661cfb6dbc97a55c6db5119fb9ae2d
Mutugi <colrium@gmail.com>
2020-11-11 16:22:56 +0300

    11 Nov 2020 16:19

---
+++ src/components/ViewPortSensor - Copy/index.js
@@ -0,0 +1,7 @@
+import withViewPortSensor from './withViewPortSensor';
+
+export const customProps = ['inViewport', 'enterCount', 'leaveCount'];
+
+export default withViewPortSensor;
+export { default as withViewPortSensor } from './withViewPortSensor';
+export { default as useInViewport } from './useInViewport';
---
+++ src/components/ViewPortSensor - Copy/useInViewport.js
@@ -0,0 +1,93 @@
+// React hooks
+import { useEffect, useRef, useState } from 'react';
+import { findDOMNode } from 'react-dom';
+
+const useInViewport = (target, options, config = { disconnectOnLeave: false }, props) => {
+	const { onEnterViewport, onLeaveViewport } = props;
+	const [, forceUpdate] = useState();
+
+	const observer = useRef();
+
+	const inViewportRef = useRef(false);
+	const intersected = useRef(false);
+
+	const enterCountRef = useRef(0);
+	const leaveCountRef = useRef(0);
+
+	function startObserver() {
+		if (target.current && observer.current) {
+			const node = findDOMNode(target.current);
+			if (node) {
+				observer.current.observe(node);
+			}
+		}
+	}
+
+	function stopObserver() {
+		if (target.current && observer.current) {
+			const node = findDOMNode(target.current);
+			if (node) {
+				observer.current.unobserve(node);
+				observer.current.disconnect();
+				observer.current = null;
+			}
+		}
+	}
+
+	function handleIntersection(entries) {
+		const entry = entries[0] || {};
+		const { isIntersecting, intersectionRatio } = entry;
+		const isInViewport = typeof isIntersecting !== 'undefined' ? isIntersecting : intersectionRatio > 0;
+
+		// enter
+		if (!intersected.current && isInViewport) {
+			intersected.current = true;
+			onEnterViewport && onEnterViewport();
+			enterCountRef.current += 1;
+			inViewportRef.current = isInViewport;
+			forceUpdate(isInViewport);
+			return;
+		}
+
+		// leave
+		if (intersected.current && !isInViewport) {
+			intersected.current = false;
+			onLeaveViewport && onLeaveViewport();
+			if (config.disconnectOnLeave && observer.current) {
+				// disconnect obsever on leave
+				observer.current.disconnect();
+			}
+			leaveCountRef.current += 1;
+			inViewportRef.current = isInViewport;
+			forceUpdate(isInViewport);
+		}
+	}
+
+	function initIntersectionObserver() {
+		if (!observer.current) {
+			// $FlowFixMe
+			observer.current = new IntersectionObserver(handleIntersection, options);
+		}
+	}
+
+	useEffect(
+		() => {
+			// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+			initIntersectionObserver();
+			startObserver();
+
+			return () => {
+				stopObserver();
+			};
+		},
+		[target, options, config, onEnterViewport, onLeaveViewport]
+	);
+
+	return {
+		inViewport: inViewportRef.current,
+		enterCount: enterCountRef.current,
+		leaveCount: leaveCountRef.current
+	};
+};
+
+export default useInViewport;
